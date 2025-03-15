481e879e88fb56826498a32799d065d7aec40071
HaroldOsorioFlores <haroldosorio32@gmail.com>
2025-03-15 18:27:21 -0500

    first commit

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import { ReactNode } from "react";
+
+export default function MainLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <div>Harold</div>
+        <div>
+          <ul>
+            <li>Perfil</li>
+            <li>Especialidad</li>
+            <li>Trabajos</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
